dcd29f513d6d2067a3ce9e5c3f9c607e31446d26
∅AK <notactuallyoak@gmail.com>
2026-03-22 09:56:48 +0700

    run no more break block

--- Assets/Scripts/PlayerDealDamage.cs
+++ Assets/Scripts/PlayerDealDamage.cs
@@ -47,13 +47,6 @@ public class PlayerDealDamage : MonoBehaviour
         {
             if (hit.collider != null)
             {
-                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Breakable"))
-                {
-                    CameraController.Instance?.CamShake(0.15f, 0.1f);
-                    ParticleEmitter.Instance.Emit("BlockBreak", hit.collider.gameObject.transform.position, Quaternion.identity);
-                    AudioManager.Instance.PlaySFX("BlockBreak");
-                    Destroy(hit.collider.gameObject);
-                }
                 hit.collider.GetComponent<EnemyBase>()?.TakeDamage(runDamage);
                 CameraController.Instance?.CamShake(0.15f, 0.1f);
             }
